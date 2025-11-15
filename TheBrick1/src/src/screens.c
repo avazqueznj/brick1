a2c2a8f40dd2a0224259b5f860dcb61f19b47a57
Alejandro Vazquez <alejandrovazquez@yahoo.com>
2025-11-15 15:37:06 -0800

    version label updt

--- TheBrick1/src/src/screens.c
+++ TheBrick1/src/src/screens.c
@@ -1342,7 +1342,7 @@ void create_screen_login_screen() {
             lv_obj_set_style_text_color(obj, lv_color_hex(0xffffffff), LV_PART_MAIN | LV_STATE_DEFAULT);
             lv_obj_set_style_align(obj, LV_ALIGN_RIGHT_MID, LV_PART_MAIN | LV_STATE_DEFAULT);
             lv_obj_set_style_text_align(obj, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
-            lv_label_set_text(obj, "v 1.2");
+            lv_label_set_text(obj, "v 1.3");
         }
         {
             // CLOCK_LOGIN
